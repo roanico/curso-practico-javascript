1f89f393258e3d2bd0c1a53cee4f34088e9ef95f
roanico <nicolasroa90@hotmail.com>
2021-07-05 14:22:27 -0400

    probando agregar cambios

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # curso-practico-javascript
 Curso Platzi practico javascript
+
+...
+
+## Taller #1: figuras geométricas
+
+- Primer paso: definir las fórmulas
+- Segundo paso: implementar las fórmulas en JavaScript
+- Tercer paso: crear funciones
+- Cuarto paso: integrar JavaScript con HTML
+
---
+++ figuras.js
@@ -0,0 +1,25 @@
+// Código del cuadrado
+console.group("Cuadrados");
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado:"+ ladoCuadrado + "cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perímetro del cuadrado es:"+ perimetroCuadrado + "cm");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El área del cuadrado es:"+ areaCuadrado + "cm²");
+congole.groupEnd();
+// Código del triángulo
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+console.log(
+    "Los lados del triángulo miden: "
+    + ladoTriangulo1 
+    + "cm, " 
+    + ladoTriangulo2 
+    + "cm, " 
+    + baseTriangulo 
+    + "cm"
+);
+
